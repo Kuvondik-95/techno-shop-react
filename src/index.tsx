e428e26cefb62ed25bf90133c5cf15587e751553
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-08 17:18:25 +0900

    feat: MUI customized intergration

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,13 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+
+import theme from "./app/MaterialTheme";
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+import './css/index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,7 +18,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <App /> 
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
