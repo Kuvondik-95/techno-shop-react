c91db0a35044b02f87ae034173e308a7efcfc31f
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-15 01:20:52 +0900

    feat: publishing helpPage screen component

--- src/app/screens/helpPage/index.tsx
+++ src/app/screens/helpPage/index.tsx
@@ -12,6 +12,13 @@ import TabPanel from '@material-ui/lab/TabPanel';
 import "../../../css/help.css";
 import { faq } from "../../../libs/data/faq";
 import { terms } from "../../../libs/data/terms";
+import HomeIcon from '@mui/icons-material/Home';
+import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+
+
 
 export default function HelpPage() {
   const [value, setValue] = React.useState("1");
@@ -23,6 +30,45 @@ export default function HelpPage() {
 
   return (
     <div className={"help-page"}>
+      <Typography className="addr-title">Our Address</Typography>
+      <Container className={"our-address-container"}>
+        <Box className="address-box">
+          <Typography>
+            <HomeIcon className="icon"/><b>Ulsan, Mugodong, Samhoe, St.12</b>
+          </Typography>
+          <Typography>
+            <QueryBuilderIcon className="icon"/>  
+            <b>Mn.-Fr.:</b> from <b>10:00</b>am to <b>21:00</b>pm
+          </Typography>
+          <Typography>
+            <QueryBuilderIcon className="icon"/>  
+            <b>Saturday:</b> from <b>10:00</b>am to <b>14:00</b>pm
+          </Typography>
+          <Typography>
+            <PhoneAndroidIcon className="icon"/>  
+            010-7217-4114
+          </Typography>
+          <Typography>
+            <LocalPhoneIcon className="icon"/>  
+            052-7217-4114
+          </Typography>
+          <Typography>
+            <EmailIcon className="icon"/>  
+            <b>Email:</b> techno-shop@gmail.com
+          </Typography>
+        </Box>
+        <Box className="location-box">
+          <iframe 
+            className="location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d626.1733686618442!2d129.26268564359393!3d35.54933445045533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35662d71568c3691%3A0x7914b89c9b7cdda4!2sDunkin&#39;!5e0!3m2!1sen!2skr!4v1752507945897!5m2!1sen!2skr"  
+            height="450" 
+            style={{border:0}} loading="lazy"
+          >
+          </iframe>
+        </Box>
+      </Container>
+      
+      <Typography className="help-title">Help</Typography>
       <Container className={"help-container"}>
         <TabContext value={value}>
           <Box className={"help-menu"}>
