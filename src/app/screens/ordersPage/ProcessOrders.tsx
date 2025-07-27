5bc21ed9c0beeca066e912ffa77866079ee6f015
Kuvondik-95 <kuvondik9595@gmail.com>
2025-07-27 15:36:51 +0500

    fix: modify processOrders and fetching data via selector

--- src/app/screens/ordersPage/ProcessOrders.tsx
+++ src/app/screens/ordersPage/ProcessOrders.tsx
@@ -3,26 +3,82 @@ import { Box, Button, Stack, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import TabPanel from '@material-ui/lab/TabPanel';
 
+import moment from "moment";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveProcessOrders } from "./selector";
+import { Product } from "../../../libs/types/product";
+import { Messages, serverApi } from "../../../libs/config";
+import { Order, OrderItem, OrderUpdateInput } from "../../../libs/types/order";
+import { useGlobals } from "../../hooks/useGlobals";
+import { OrderStatus } from "../../../libs/enums/order.enum";
+import OrderService from "../../services/Order.Service";
+import { sweetErrorHandling } from "../../../libs/sweetAlert";
+import { T } from "../../../libs/types/common";
+
+/** REDUX SLICE & SELECTOR **/
+const processOrderRetriever = createSelector(
+  retrieveProcessOrders,
+  (processOrders) => ({ processOrders })
+);
+
+
 interface ProcessOrdersProps{
   setValue: (input:string) => void;
 }
 export function ProcessOrders( props: ProcessOrdersProps){
   const { setValue } = props;
+  const {authMember, setOrderBuilder} = useGlobals();
+  const { processOrders } = useSelector(processOrderRetriever);
+
+  /** HANDLERS **/
+  const finishOrderHandler = async (e: T) => {
+    try{
+      if(!authMember) throw new Error(Messages.error2);
+      // PAYMENT PROCESS 
+
+      const orderId = e.target.value;
+      const input: OrderUpdateInput = {
+        orderId: orderId,
+        orderStatus: OrderStatus.FINISH
+      };
+
+      const confirmation = window.confirm("Have you received your order?")
+      if(confirmation){
+        const order = new OrderService();
+        await order.updateOrder(input);
+        
+        // moving from PAUSED => PROCESS ORDER TAB
+        setValue("3");
+        // ORDER REBUILD
+        setOrderBuilder(new Date());
+      }
+    }catch(err){
+      console.log(err)
+      sweetErrorHandling(err).then();
+    }
+  }
+
   return (
           <TabPanel value={"2"} className={"order-tab-panel"}>
-            {[1].map((order, index) => {
+            {processOrders.map((order:Order) => {
               return(
-                <Stack className="process-orders-frame" key={index}>
-                  {[1,2].map((item) => {
+                <Stack className="process-orders-frame" key={order._id}>
+                  {order?.orderItems?.map((item) => {
+                    const product: Product = order.productData.filter(
+                      (ele: Product) => {
+                        return item.productId === ele._id;
+                    })[0];
+                    const imagePath = `${serverApi}/${product.productImages[0]}`;
                     return (
                       <Stack className="process-orders">
                         <Stack className="process-orders-info">
-                          <img className={"product-img"} src={"/img/phoneImg/main.png"} alt="" />
-                          <Box className="product-name">Apple</Box>
+                          <img className={"product-img"} src={imagePath} alt="" />
+                          <Box className="product-name">{product.productName}</Box>
                         </Stack>
                         
                         <Box className="process-orders-count"> 
-                          $400 &nbsp; x &nbsp; 2 &nbsp; = &nbsp; $800
+                          ${item.itemPrice} &nbsp; x &nbsp; {item.itemQuantity} &nbsp; = &nbsp; ${item.itemQuantity * item.itemPrice}
                         </Box>
                       </Stack>
                     )
@@ -31,17 +87,16 @@ export function ProcessOrders( props: ProcessOrdersProps){
 
                   <Stack className={"total-price"}>
                     <Box className={"summ"}>
-                      Product price &nbsp; &nbsp; $800 &nbsp; <AddIcon className={"count-icon"}/> &nbsp; 
-                      Delivery cost &nbsp; &nbsp; $10 &nbsp; &nbsp; = &nbsp; &nbsp; 
-                      Total &nbsp;&nbsp;&nbsp;   
+                      Product price &nbsp; &nbsp; ${order.orderTotal - order.orderDelivery} &nbsp; <AddIcon className={"count-icon"}/> &nbsp; Delivery cost &nbsp; &nbsp; ${order.orderDelivery} &nbsp; &nbsp; = &nbsp; &nbsp; Total &nbsp;&nbsp;&nbsp; ${order.orderTotal}   
                     </Box>
 
                     <Stack className={"buttons-box"}>
                       <Typography className={"data-compl"}>
-                        hour
+                        {moment().format("YY-MM-DD HH:mm")}
                       </Typography>
                       <Button 
                         className={"payment-btn"}
+                        onClick={finishOrderHandler}
                         >
                         VERIFY TO FULLFILL
                       </Button>
@@ -51,7 +106,7 @@ export function ProcessOrders( props: ProcessOrdersProps){
               )
             })}
                 
-            {[1,2].length === 0 && (
+            {!processOrders || (processOrders.length === 0) && (
               <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
                 <img 
                   src={"/icons/noimage-list.svg"} 
